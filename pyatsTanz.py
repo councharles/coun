a7154d16405a7a2fde6bfb01904880bd81499983
councharles <councharles@yahoo.com>
2026-05-17 20:21:12 +0100

    Remove all Files

--- pyatsTanz.py
+++
@@ -1,9 +0,0 @@
-from pyats.topology import loader
-#from genie.testbed import load
-
-testbed = loader.load('myfiles/testbed.yaml')
-for device in testbed.devices.values():
-    device.connect(init_exec_commands=['terminal length 0'],init_config_commands=[])
-    out = device.execute('sh run int lo0')
-    #out = device.parse('sh run int lo0')
-    print(out)
